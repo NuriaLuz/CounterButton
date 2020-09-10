22c69a8cde5aa084e8656a698fc7381271dd827a
Nuria Rodriguez <nurialuzr1@gmail.com>
2020-09-10 01:42:57 -0400

    counter button works

--- app/index.js
+++ app/index.js
@@ -1,1 +1,38 @@
-// Your code here!
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Counter extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            count:0
+        }
+        this.increment = this.increment.bind(this)
+    }
+    increment(){
+        this.setState({
+            count:this.state.count + 1
+        })
+    }
+    render(){
+        return(
+            <div id='container'>
+            <div id='navabar'>
+            Counter.js
+            </div>
+            
+            <div id='container'>
+            <h1>{this.state.count}</h1>
+            <button onClick={this.increment}>Increment</button>
+
+            </div>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(
+    <Counter/>,
+    document.getElementById('app')
+)
