33aa7f54fc922238fa8bcbdb37914e9e0fd49d31
Nuria Rodriguez <nurialuzr1@gmail.com>
2020-09-10 02:43:22 -0400

    added decrement button

--- app/index.js
+++ app/index.js
@@ -8,16 +8,22 @@ class Counter extends React.Component{
             count:0
         }
         this.increment = this.increment.bind(this)
+        this.decrement = this.decrement.bind(this)
     }
     increment(){
         this.setState({
             count:this.state.count + 1
         })
     }
+    decrement(){
+        this.setState({
+            count:this.state.count - 1
+        })
+    }
     render(){
         return(
             <div id='container'>
-
+            <div id='inner'>
             <div id='navbar'>
             <h1>Let's Count!</h1>
             </div>
@@ -25,7 +31,9 @@ class Counter extends React.Component{
             <div id='counter'>
             <h1>{this.state.count}</h1>
             <button onClick={this.increment}>Increment</button>
-
+            <br></br>
+            <button onClick={this.decrement}>Decrement</button>
+            </div>
             </div>
             </div>
         )
